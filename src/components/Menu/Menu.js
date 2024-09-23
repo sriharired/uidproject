3620abf74eafca95d605f43010185d9cceb4596b
sriharired <psrihari1952@gmail.com>
2024-09-23 15:22:34 -0400

    commit

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,5 +1,10 @@
 import React, { useState } from 'react';
-import './Menu.css'; 
+import './Menu.css';
+import Introcuction from '../Section1/Section1';
+import TeamMembers from '../Section2/Section2';
+import ProejctDescription from '../Section3/Section3';
+import Problemstatement from '../Section4/Section4';
+import Technologies from '../Section5/Section5';
 
 const Menu = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -19,11 +24,11 @@ const Menu = () => {
         <div className="menu-line"></div>
       </div>
       <ul className={`nav-links ${isOpen ? 'open' : ''}`}>
-        <li><a href="#home">Introduction</a></li>
-        <li><a href="#services">Team Members</a></li>
-        <li><a href="#about">Project Description</a></li>
-        <li><a href="#contact">Problem Statement</a></li>
-        <li><a href="#contact">Technologies Used</a></li>
+        <li><a href={Introcuction}>Introduction</a></li>
+        <li><a href={TeamMembers}>Team Members</a></li>
+        <li><a href={ProejctDescription}>Project Description</a></li>
+        <li><a href={Problemstatement}>Problem Statement</a></li>
+        <li><a href={Technologies}>Technologies Used</a></li>
       </ul>
     </nav>
   );
