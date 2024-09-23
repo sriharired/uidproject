487258a6e7672141c109f95c8beb610d211b9aac
sriharired <psrihari1952@gmail.com>
2024-09-23 19:17:58 -0400

    commit

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <div className="footer-bottom">
       <p>Designed and Developed by Putha Sri Hari Reddy ,Srinivas Amla & Keerthi Kalakoti</p>
-      <p>&copy; 2024 MyBrand. All rights reserved.</p>
+      <p>&copy; 2024 UID. All rights reserved.</p>
     </div>
   );
 };
