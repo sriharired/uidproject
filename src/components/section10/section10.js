676ad7230553d7ea20156d3df82ff7fa1e27112d
sriharired <psrihari1952@gmail.com>
2024-12-08 22:03:39 -0500

    updated successfully

--- src/components/section10/section10.js
+++ src/components/section10/section10.js
@@ -117,7 +117,7 @@ const ProjectSection = () => {
             1. Clone the repository from the link provided by using{" "}
             <code>git clone "enter-git-link-here"</code>
             <br />
-            2. Navigate to project folder using {" "} <code>cd IHM</code>
+            2. Navigate to project folder using {" "} <code>cd IHM</code> <br/>
             3. Install the required dependencies by running{" "}
             <code>npm install</code>.
             <br />
